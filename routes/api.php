901c7a39403f939b5da2ac74f05a47da01e5308a
KyeBuff <kye.buffery@btinternet.com>
2017-12-20 13:08:07 +0000

    created api routes and api controller and database table

--- routes/api.php
+++ routes/api.php
@@ -2,17 +2,14 @@
 
 use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+$router->post("tasks", "Tasks@create");
+$router->get("tasks", "Tasks@list");
+$router->put("tasks/{task}", "Tasks@update");
+$router->delete("tasks/{task}", "Tasks@delete");
+$router->patch("tasks/{task}/complete", "Tasks@complete");
